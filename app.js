340df82b806214997138923d4c347b5f831729e3
Washington <lokalawashington2019@gmail.com>
2023-01-13 13:03:46 +0300

    routing with an a array of objects done

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const { title } = require('process');
+const bookRouter = express.Router();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,8 +17,36 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist'))
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+var books = [{
+  title: 'CEO',
+  genre: 'Biography',
+  author: 'Washington',
+  read: false
+}];
+
+bookRouter.route('/').get((req, res) => {
+  res.render('books',
+    {
+      nav: [{ link: '/books', title: 'Services' },
+      { link: '/authors', title: 'Authors' }],
+      title: 'Services',
+      books
+    })
+});
+
+bookRouter.route('/single').get((req, res) => {
+  res.send('Single books')
+});
+
+app.use('/books', bookRouter);
+
 app.get('/', (req, res) => {
-  res.render('index', { list: ['a', 'b'], title: 'NyumbaKumiApp Hub' });
+  res.render('index', {
+    nav: [{ link: '/books', title: 'Services' },
+    { link: '/authors', title: 'Authors' }],
+    title: 'NyumbaKumiApp Hub'
+  }
+  );
 });
 
 app.listen(port, () => {
